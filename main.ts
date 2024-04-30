25ec184de54f4ea0809da3549a37e40abafee1cb
Mani Mohan <hi@mani.ink>
2024-04-30 12:19:39 -0700

    Updates for community plugin guidelines
    1. minAppVersion updated in manifest.json
    2. fundingURL updated
    3. Updated UI elements to sentence case.
    4. Removed sample plugin residual code.

--- main.ts
+++ main.ts
@@ -50,7 +50,7 @@ export default class OLocalLLMPlugin extends Plugin {
 
 			menu.addItem((item) =>
 				item
-					.setTitle("Make it Professional")
+					.setTitle("Make it professional")
 					.setIcon("school")
 					.onClick(async () => {
 						let selectedText = this.getSelectedText();
@@ -77,23 +77,11 @@ export default class OLocalLLMPlugin extends Plugin {
 			menu.showAtMouseEvent(event);
 		});
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText("Local LLM Helper running");
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new OLLMSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
 	}
 
 	private getSelectedText() {
@@ -151,7 +139,7 @@ class OLLMSettingTab extends PluginSettingTab {
         containerEl.empty();
 
         new Setting(containerEl)
-            .setName("Server Address")
+            .setName("Server address")
             .setDesc("localhost or remote (do not include http://)")
             .addText((text) =>
                 text
@@ -165,7 +153,7 @@ class OLLMSettingTab extends PluginSettingTab {
 
         // Add a new setting for another text input
         new Setting(containerEl)
-            .setName("Server Port")
+            .setName("Server port")
             .setDesc("Port number for the LLM server")
             .addText((text) =>
                 text
@@ -178,7 +166,7 @@ class OLLMSettingTab extends PluginSettingTab {
             );
 		
 		new Setting(containerEl)
-            .setName("LLM Model")
+            .setName("LLM model")
             .setDesc("currently works with LM Studio - find model name there")
             .addText((text) =>
                 text
